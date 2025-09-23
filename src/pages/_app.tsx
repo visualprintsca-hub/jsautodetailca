34d304235a719d84f2d7fb8f4581fe4f229e00c8
Visual Prints <visualprints.ca@gmail.com>
2025-09-23 16:29:35 -0700

    hero complete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link href="https://api.fontshare.com/v2/css?f[]=nippo@200,300,400,500,700&display=swap" rel="stylesheet"/>
+        <link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701,900,901&display=swap" rel="stylesheet"/>
       </Head>
       <AppProvider i18n={{}}>
         <Header/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Button from '@/components/button';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
+
+
+
 export default function Home() {
   return (
     <div
@@ -18,17 +24,15 @@ export default function Home() {
       <main className="flex flex-col">
         <section className="h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center" style={{backgroundImage: 'url(/hero1.png)'}}>
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl h-full py-32 flex flex-col  justify-end">
-            <div className="text-left text-white flex flex-col justify-start items-start max-w-200">
-              <h1 className="text-2xl md:text-5xl text-left font-bold text-white mb-6 italic uppercase" style={{fontFamily: 'Nippo'}}>
+            <div className="text-left text-white flex flex-col gap-4 justify-start items-start max-w-200">
+              <div className="flex flex-col gap-3">
+              <h1 className="text-2xl md:text-5xl text-left font-bold text-white  italic uppercase" style={{fontFamily: 'Nippo'}}>
               Your Car's Best Clean
               — Professional, Affordable, Sustainable.
               </h1>
               <p className="text-lg md:text-xl text-white/90 mb-8 max-w-2xl text-left">
-                Professional mobile auto detailing services at your location
-              </p>
-              <button className="bg-fuchsia-700 hover:bg-fuchsia-600 text-white font-semibold py-3 px-8 rounded-full transition-colors duration-200">
-                Book Now
-              </button>
+              We make your car shine inside and out, without breaking the bank or harming the planet.</p></div>
+             <Button variant="primary">Book Your Detail Today</Button>
             </div>
           </div>
         </section>
