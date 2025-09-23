041dddeda964049f88a84662a36ba6a75119a5df
Visual Prints <visualprints.ca@gmail.com>
2025-09-23 16:32:33 -0700

    removed shopify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Header from "@/components/header";
-import {AppProvider} from '@shopify/polaris';
 // import '@shopify/polaris/build/esm/styles.css';
 import "@/styles/globals.css";
 
@@ -13,10 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <link href="https://api.fontshare.com/v2/css?f[]=nippo@200,300,400,500,700&display=swap" rel="stylesheet"/>
         <link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701,900,901&display=swap" rel="stylesheet"/>
       </Head>
-      <AppProvider i18n={{}}>
+   
         <Header/>
         <Component {...pageProps} />
-      </AppProvider>
+      
     </>
   )
 }
